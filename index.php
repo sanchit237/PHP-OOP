c188c1e680bce4e35d972c0cc678f28f60a88f0a
sanchit237 <sanchitnemlekar8@gmail.com>
2022-04-05 22:34:58 +0530

    Select functionality added

--- index.php
+++ index.php
@@ -5,22 +5,22 @@ include "Database.php";
 $db = new Database();
 
 //Insert
-// $db->insert('students', ['name'=>'sanchit','age'=>25,'city'=>'mumbai']);
+$db->insert('students', ['name'=>'sanchit','age'=>25,'city'=>'mumbai']);
 
-// echo "Inserted results are:";
-// print_r($db->getresult());
+echo "Inserted results are:";
+print_r($db->getresult());
 
 //Update
-// $db->update('students', ['name'=>'test','age'=>20,'city'=>'pune'],'id="3"');
+$db->update('students', ['name'=>'test','age'=>20,'city'=>'pune'],'id="3"');
 
-// echo "Updated results are:";
-// print_r($db->getresult());
+echo "Updated results are:";
+print_r($db->getresult());
 
 //Delete
-// $db->delete('students', 'id="3"');
+$db->delete('students', 'id="3"');
 
-// echo "Deleted results are:";
-// print_r($db->getresult());
+echo "Deleted results are:";
+print_r($db->getresult());
 
 //Select
 $db->select('students', ['name','city','age'],null,'name'); 
