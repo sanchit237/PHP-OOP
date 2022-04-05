c719c1e433211d5c4d78d6c0de4b2f8bf8f2bd1e
sanchit237 <sanchitnemlekar8@gmail.com>
2022-04-05 22:38:30 +0530

    prettifying data

--- index.php
+++ index.php
@@ -6,25 +6,30 @@ $db = new Database();
 
 //Insert
 $db->insert('students', ['name'=>'sanchit','age'=>25,'city'=>'mumbai']);
-
 echo "Inserted results are:";
+
+echo "<pre>";
 print_r($db->getresult());
+echo "</pre>";
 
 //Update
 $db->update('students', ['name'=>'test','age'=>20,'city'=>'pune'],'id="3"');
-
 echo "Updated results are:";
+
+echo "<pre>";
 print_r($db->getresult());
+echo "</pre>";
 
 //Delete
 $db->delete('students', 'id="3"');
-
 echo "Deleted results are:";
+
+echo "<pre>";
 print_r($db->getresult());
+echo "</pre>";
 
 //Select
 $db->select('students', ['name','city','age'],null,'name'); 
-
 echo "Selected results are:";
 
 echo "<pre>";
